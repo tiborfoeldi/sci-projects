e33ef56caee159b565d6473568559c6cbe29ef2b
Tibor Foldi <tiborfoeldidev@gmail.com>
2018-08-28 15:54:06 +0300

    corrected function homework

--- functions/src/src/main/java/ro/sci/functions/Main.java
+++ functions/src/src/main/java/ro/sci/functions/Main.java
@@ -4,8 +4,10 @@ import ro.sci.functions.math.Calculator;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("salutare");
 
         Calculator calculator = new Calculator();
-
+        System.out.println("suma: " + calculator.aduna(3,4));
+        System.out.println("fibonacci: " + calculator.fibonacci(10));
     }
 }
--- functions/src/src/main/java/ro/sci/functions/math/Calculator.java
+++ functions/src/src/main/java/ro/sci/functions/math/Calculator.java
@@ -1,135 +1,22 @@
 package ro.sci.functions.math;
 
-import ro.sci.functions.DayInMonth.DaysInMonthsCalculator;
-
-import java.util.Scanner;
-
 /**
  * Calculator utility.
  */
-public class Calculator extends DaysInMonthsCalculator {
-
-    DaysInMonthsCalculator DInM = new DaysInMonthsCalculator();
-    Scanner sc = new Scanner(System.in);
-
-    /**
-     * Be advised... no exception handling, invalid input will terminate the flow
-     * If number is asked try to input a number
-     * If String input is needed, do so
-     */
-
-
-    public Calculator() {
-        menu();
-        boolean quit = false;
-        while (!quit) {
-            int choice = sc.nextInt();
-            if (choice == 6 || choice == 7 || choice == 8) {
-                switch (choice) {
-                    case 6:
-                        menu();
-                        break;
-                    case 7:
-                        DInM.days();
-                        choice();
-                        break;
-                    case 8:
-                        quit = true;
-                    break;
-                }
-            } else {
-
-                System.out.print("Enter first number: ");
-                int x = sc.nextInt();
-                System.out.print("Enter second number: ");
-                int y = sc.nextInt();
-                switch (choice) {
-                    case 1:
-                        aduna(x, y);
-                        choice();
-                        break;
-                    case 2:
-                        substraction(x, y);
-                        choice();
-                        break;
-                    case 3:
-                        multiply(x, y);
-                        choice();
-                        break;
-                    case 4:
-                        divide(x, y);
-                        choice();
-                        break;
-                    case 5:
-                        System.out.println("Fibonacci " + x + " = " + fibonacci(x));
-                        System.out.println("Fibonacci " + y + " = " + fibonacci(y));
-                        choice();
-                        break;
-                }
-            }
-        }
-    }
+public class Calculator {
 
     /**
      * Function that calculates the sum of two integer numbers
-     *
      * @param x
      * @param y
      * @return the sum
      */
     public int aduna(int x, int y) {
-        int result = x + y;
-        System.out.println(x + " + " + y + " = " + result);
-        return result;
+        return x + y;
     }
-
-    /**
-     * Function that substracts an integer value from another integer value
-     *
-     * @param x
-     * @param y
-     * @return y substracted from x
-     */
-
-    public int substraction(int x, int y) {
-        int result = x - y;
-        System.out.println(x + " - " + y + " = " + result);
-        return result;
-    }
-
-    /**
-     * Function that calculates the multiplication of 2 integer values
-     *
-     * @param x
-     * @param y
-     * @return x multiplied by y
-     */
-
-    public int multiply(int x, int y) {
-        int result = x * y;
-        System.out.println(x + " * " + y + " = " + result);
-        return result;
-    }
-
-
-    /**
-     * Function that calculates the division of two integer values
-     *
-     * @param x
-     * @param y
-     * @return x divided by y
-     */
-
-    public double divide(int x, int y) {
-        int result = x / y;
-        System.out.println(x + " / " + y + " = " + result);
-        return result;
-    }
-
 
     /**
      * Function that calculates fibonacci sum
-     *
      * @param n - the number to calculate fibonacci for
      * @return fibonacci sum
      */
@@ -139,15 +26,4 @@ public class Calculator extends DaysInMonthsCalculator {
         }
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
-
-    public void choice() {
-        System.out.print("Enter your choice: ");
-    }
-
-    public void menu() {
-        System.out.print("Choose: \n 1. for addition \n 2. for substraction \n 3. for multiplication \n 4. for division \n 5. for fibonacci  \n 6. to print out menu options \n 7. to get days in month \n 8. to exit \n Enter your choice: ");
-    }
-
-
-
 }
