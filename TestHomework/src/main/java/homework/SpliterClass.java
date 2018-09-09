51a1acb4786065d9ffe8a5d2d5f36736b4c0b22b
Tibor Foldi <tiborfoeldidev@gmail.com>
2018-09-09 09:46:50 +0300

    Junit homework

--- TestHomework/src/main/java/homework/SpliterClass.java
+++ TestHomework/src/main/java/homework/SpliterClass.java
@@ -1,6 +1,39 @@
 package homework;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SpliterClass {
+
+    Map<String, String> list = new HashMap<String, String>();
+
+    public String[] sentenceSpitter(String sentance) {
+        String[] splitedSentance = sentance.toLowerCase().split(" ");
+        return splitedSentance;
+    }
+
+
+    public String[] changeWritenNumbersToDigits(String[] array, Map<String, String> toVerify) {
+
+        for (int i = 0; i < array.length; i++) {
+            if (toVerify.containsKey(array[i])) {
+                array[i] = toVerify.get(array[i]);
+            }
+        }
+        return array;
+    }
+
+    public String printChangedSentence(String[] array) {
+        String sentance = "";
+        for (int i = 0; i < array.length; i++) {
+            if (i == array.length - 1) {
+                sentance += array[i];
+            } else {
+                sentance += array[i] + " ";
+            }
+        }
+        return sentance;
+    }
 
 
 }
