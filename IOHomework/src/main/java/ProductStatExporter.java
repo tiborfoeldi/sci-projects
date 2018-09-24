7f687d53b5dd5f18a00319b465002c338099bcc1
Földi Tibor <33400151+tiborfoeldi@users.noreply.github.com>
2018-09-24 07:58:25 +0300

    Update ProductStatExporter.java

--- IOHomework/src/main/java/ProductStatExporter.java
+++ IOHomework/src/main/java/ProductStatExporter.java
@@ -18,18 +18,30 @@ public class ProductStatExporter {
             List<Customer> customersByMoneySpent = custStats.getCustomerByMostMoneySpent();
             bw.append("Best Selling Products" + "\n");
             for (int i = 0; i < products.size(); i++) {
+                if (i<10) {
                 bw.append("\n" + (i + 1) + ". " + products.get(i).getName() + " - " + products.get(i).getSoldPiecesCounter() + " Units sold!");
                 bw.newLine();
+                } else { 
+                    break;
+                }
             }
             bw.append("\n" + "Customers with most Orders:" + "\n");
             for (int i = 0; i < customerByNrOfOrders.size(); i++){
+                if (i<10) {
                 bw.append("\n" + (i+1) + ". " + customerByNrOfOrders.get(i).getName() + " - " + customerByNrOfOrders.get(i).getNrOfOrders() + " orders");
                 bw.newLine();
+                } else {
+                    break;
+                }
             }
             bw.append("\n" + "Top Customers" + "\n");
             for(int i = 0; i<customersByMoneySpent.size(); i++){
+                if (i<10) {
                 bw.append("\n" + (i+1) + ". " + customersByMoneySpent.get(i).getName() + " - $" + customersByMoneySpent.get(i).getMoneySpent());
                 bw.newLine();
+                } else {
+                    break;
+                }
             }
 
 
